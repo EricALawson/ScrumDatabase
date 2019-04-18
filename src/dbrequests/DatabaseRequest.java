49a85ca16035c54151f0a547d4194635fd1deb39
ericlawson <eric.alexander.lawson@gmail.com>
2019-04-17 18:08:00 -0700

    Initial commit

---
+++ src/dbrequests/DatabaseRequest.java
@@ -0,0 +1,71 @@
+package dbrequests;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public abstract class DatabaseRequest {
+	private static ArrayList<DatabaseRequest> allRequests = new ArrayList<>();
+	protected static Connection conn;
+	
+	protected PreparedStatement prepStmnt;
+	protected String description;
+
+	public DatabaseRequest() {
+		allRequests.add(this);
+	}
+	
+	public abstract void execute() throws SQLException;
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public static ArrayList<DatabaseRequest> getAllRequests() {
+		return allRequests;
+	}
+	
+	public static void setConnection(Connection newConn) {
+		conn = newConn;
+	}
+
+	public static void initializeRequests() {
+		try {
+			new RequestListAllTeams();
+			new RequestListTeamByID();
+			new RequestMeetingByID();
+			new RequestRoomsOnDate();
+			new RequestCreateMeeting();
+			new RequestMoveMeetingsToNewRoom();
+			new RequestMeetingsByPurposeAndDate();
+			new RequestCreateSoftwareTeam();
+		} catch (SQLException ex) {
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			ex.printStackTrace();
+		}
+		
+	}
+	
+	protected void printResultSet(ResultSet results) throws SQLException {
+		ResultSetMetaData rsmd = results.getMetaData();
+		int numberCols = rsmd.getColumnCount();
+		for (int i = 1; i <= numberCols; i++) {
+			System.out.print(rsmd.getColumnLabel(i) + "\t\t");
+		}
+		System.out.println("\n---------------------------");
+		while (results.next()) {
+			for (int i = 1; i <= rsmd.getColumnCount(); i ++) {
+				if (i > 1) System.out.print(",\t\t");
+				String colValue = results.getString(i);
+				System.out.print(colValue);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
