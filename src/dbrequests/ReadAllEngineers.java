8bc49cab8ca5c5bc85ea6500e1c86304629d0816
1234567890ROBIN <48961722+1234567890ROBIN@users.noreply.github.com>
2019-05-01 16:28:21 -0700

    Update ReadAllEngineers.java

--- src/dbrequests/ReadAllEngineers.java
+++ src/dbrequests/ReadAllEngineers.java
@@ -1,19 +1,30 @@
 package dbrequests;
 
-
 import java.sql.SQLException;
-import util.InputValidator;
+
+import java.sql.ResultSet;
 
 public class ReadAllEngineers extends DatabaseRequest {
 
 	public ReadAllEngineers() throws SQLException {
-		description = "Read All Engineers";
-		String sql = "SELECT * FROM SoftwareEngineers;";
-		prepStmnt = conn.prepareStatement(sql);
+		String sql = "SELECT * FROM SoftwareEngineers";
+		stmnt = conn.createStatement();
+		resultSet = stmnt.executeQuery(sql);
 	}
 	
-	public void execute() throws SQLException {
-		prepStmnt.executeQuery();
+	public static void list() throws SQLException {
+		
+		System.out.println("TeamName\tEmployeeID\tSprintID");
+		while(resultSet.next()) {
+			String TeamName = resultSet.getString(1);
+			String EmployeeID = resultSet.getString(2);
+			String SprintID = resultSet.getString(3);
+		
+			System.out.println(TeamName + "\t" + EmployeeID + "\t" + SprintID);
+		}
 	}
 
-}
+
+	public void execute() throws SQLException {
+		list();
+	}
