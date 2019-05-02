ed6fa955792c5550ee1ad9770d113dadedc6d123
ericlawson <eric.alexander.lawson@gmail.com>
2019-05-01 19:28:13 -0700

    implemented some simple delete classes.

--- src/dbrequests/DeleteManagement.java
+++ src/dbrequests/DeleteManagement.java
@@ -1,16 +1,26 @@
 package dbrequests;
 
+import util.InputValidator;
+
 import java.sql.SQLException;
 
 public class DeleteManagement extends DatabaseRequest{
 
 	public DeleteManagement() throws SQLException {
 		description = "Delete a management member";
+		String sql = "DELETE FROM ManagementMembers WHERE ManagementMembers.EmployeeID = ?;";
+		prepStmnt = conn.prepareStatement(sql);
 	}
 
 	@Override
 	public void execute() throws SQLException {
-		// TODO Auto-generated method stub
+		prepStmnt.setString(1, InputValidator.getEmployeeID());
+		int deleted = prepStmnt.executeUpdate();
+		if (deleted > 0) {
+			System.out.println(deleted + "management member was deleted");
+		} else {
+			System.out.println("Failure: No matching management member found");
+		}
 		
 	}
 
--- src/dbrequests/DeleteProjectStory.java
+++ src/dbrequests/DeleteProjectStory.java
@@ -1,17 +1,26 @@
 package dbrequests;
 
+import util.InputValidator;
+
 import java.sql.SQLException;
 
 public class DeleteProjectStory extends DatabaseRequest{
 
 	public DeleteProjectStory() throws SQLException {
 		description = "Delete story from project";
+		String sql = "DELETE FROM UserStories WHERE UserStories.USID = ?";
+		prepStmnt = conn.prepareStatement(sql);
 	}
 
 	@Override
 	public void execute() throws SQLException {
-		// TODO Auto-generated method stub
-		
+		prepStmnt.setString(1, InputValidator.getUserStoryID());
+		int deleted = prepStmnt.executeUpdate();
+		if (deleted > 0) {
+			System.out.println(deleted + " user story was deleted");
+		} else {
+			System.out.println("Failure: No matching user story found");
+		}
 	}
 
 }
