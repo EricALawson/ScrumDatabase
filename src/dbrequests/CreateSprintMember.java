e4d70753a42fcf558f91a4607e2597339207e4cd
1234567890ROBIN <48961722+1234567890ROBIN@users.noreply.github.com>
2019-05-01 18:22:40 -0700

    Update CreateSprintMember.java

--- src/dbrequests/CreateSprintMember.java
+++ src/dbrequests/CreateSprintMember.java
@@ -1,5 +1,26 @@
 package dbrequests;
 
-public class CreateSprintMember {
+import java.sql.SQLException;
+
+import util.InputValidator;
+
+public class CreateSprintMember extends DatabaseRequest{
+	
+	public CreateSprintMember() throws SQLException {
+		description = "Create Sprint Member";
+		String updateSQL = "INSERT INTO SprintTeamMembers  (TeamName,SprintID,EmployeeID) VALUES(?,?,?)";
+		prepStmnt = conn.prepareStatement(updateSQL);
+		
+		
+	}
+
+	@Override
+	public void execute() throws SQLException {
+		prepStmnt.setString(1, InputValidator.getTeamName());
+		prepStmnt.setString(2, InputValidator.getSprintID());
+		prepStmnt.setString(3, InputValidator.getEmployeeID());
+		prepStmnt.executeUpdate();
+		
+	}
 
 }
