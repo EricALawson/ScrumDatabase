c1f37a91ee8c07877f36f6bd21804e9dda24470e
Julie Do <49849435+jelliciousdodobird@users.noreply.github.com>
2019-05-01 18:58:51 -0700

    Update DeleteManagement.java
    I HATE THIS.

--- src/dbrequests/DeleteManagement.java
+++ src/dbrequests/DeleteManagement.java
@@ -3,11 +3,35 @@ package dbrequests;
 import java.sql.SQLException;
 
 public class DeleteManagement extends DatabaseRequest{
-
+	
+	public DeleteManagement() {
+		description = "Remove Engineer from Project Management";
+	}
+	
 	@Override
 	public void execute() throws SQLException {
-		// TODO Auto-generated method stub
+		String findColumn = InputValidator.getManagementColumn();
+		String findCondition;
+		if (findColumn = "ProjectName") {
+			findCondition = InputValidator.getProjectName();
+		}
+		else if (findColumn = "EmployeeID") {
+			findCondition = InputValidator.getEmployeeID();
+		}
+		String sql = "DELETE FROM ManagementMembers WHERE ManagementMembers." + findColumn + " = ?;";
+		prepStmnt = conn.prepareStatement(sql);
+		prepStmnt.setString(1, findCondition);
 		
+		int rows_affected = prepStmnt.executeUpdate();
+		
+		if (rows_affected > 0) {
+			System.out.println("Success: " + rows_affected + " rows affected.");
+		} else if (rows_affected == 0) {
+			System.out.println("Failure: " + rows_affected + " rows affected.");
+		}
+		else {
+			System.out.printlm("Error: " + rows_affected + " rows affected.");
+		}
 	}
 
 }
