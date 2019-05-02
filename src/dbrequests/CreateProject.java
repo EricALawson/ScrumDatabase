ece8000a48d4ba7f1b0e92259b73fd563482f4fc
1234567890ROBIN <48961722+1234567890ROBIN@users.noreply.github.com>
2019-05-02 12:02:48 -0700

    Update CreateProject.java

--- src/dbrequests/CreateProject.java
+++ src/dbrequests/CreateProject.java
@@ -13,9 +13,17 @@ public class CreateProject extends DatabaseRequest {
 	}
 
 	public void execute() throws SQLException {
-		prepStmnt.setString(1, InputValidator.getProjectName());
-		prepStmnt.setString(2, InputValidator.getStartDate());
-		prepStmnt.setString(3, InputValidator.getEndDate());
+		String ProjectName = InputValidator.getProjectName();
+		String StartDate = InputValidator.getStartDate();
+		String EndDate = InputValidator.getEndDate();
+		prepStmnt.setString(1, ProjectName);
+		prepStmnt.setString(2, StartDate);
+		prepStmnt.setString(3, EndDate);
 		prepStmnt.executeUpdate();
+		
+		System.out.println("New  Project Has Been Created");
+		System.out.println("Project Name = " + ProjectName);
+		System.out.println("Start Date = " + StartDate);
+		System.out.println("End Date = " + EndDate);
 	}
 }
