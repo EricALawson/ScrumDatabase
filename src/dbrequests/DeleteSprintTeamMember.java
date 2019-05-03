dc2b54faece26fcd661743a5995562aff457f750
izzykid <loveizzydinh@gmail.com>
2019-05-02 23:19:26 -0700

    exception handling

--- src/dbrequests/DeleteSprintTeamMember.java
+++ src/dbrequests/DeleteSprintTeamMember.java
@@ -17,14 +17,20 @@ public class DeleteSprintTeamMember extends DatabaseRequest{
 		prepStmnt.setString(1, InputValidator.getTeamName());
 		prepStmnt.setString(2, InputValidator.getEmployeeID());
 		prepStmnt.setString(3, InputValidator.getSprintID());
-		int removed = prepStmnt.executeUpdate();
-
-		if (removed > 0) {
-			System.out.println("Success: " + removed + " rows deleted");
-		} else {
-			System.out.println("No matching team members were found.");
-		}
 		
+		try{
+			int removed = prepStmnt.executeUpdate();
+
+			if (removed > 0) {
+				System.out.println("Success: " + removed + " rows deleted");
+			} else {
+				System.out.println("No matching team members were found.");
+			}
+		}
+		catch (SQLIntegrityConstraintViolationException e){
+			System.out.println("Error: ");
+			System.out.println(e);
+		}
 	}
 
 }
